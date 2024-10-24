5a2bcf612e5f6918ba084a750524e8ff396d6201
CamilinPinguin <102928775+CamilinGarces@users.noreply.github.com>
2024-10-24 23:59:15 +0000

    cuarto commit

--- Infraestructuras-Paralelas/src/all
+++ Infraestructuras-Paralelas/src/all
@@ -1,15 +1,6 @@
 #!/usr/bin/env bash
-#
-# Este script se encarga de invocar los tres programas que permiten la 
-# conversion de un PNG a secuencia de pixeles, se procesan esos pixeles y
-# posteriormente se convierte esa secuencia de pixeles a un archivo en formato
-# PNG
-#
-# Autor: John Sanabria - john.sanabria@correounivalle.edu.co
-# Fecha: 2024-08-22
-#
 
-INPUT_DIR="./"
+INPUT_DIR="../public/imagen/"
 TEMP_FILE="image.bin"
 
 
--- Infraestructuras-Paralelas/src/fromBin2PNG.py
+++ Infraestructuras-Paralelas/src/fromBin2PNG.py
@@ -3,18 +3,28 @@ from PIL import Image
 import sys
 import os
 
-
-if len(sys.argv) == 1:
-  print("Dar nombre de archivo")
-  sys.exit(1)
+if len(sys.argv) != 2:
+    print("Dar nombre de archivo")
+    sys.exit(1)
 
 INPUT_FILE = sys.argv[1]
-FILENAME = os.path.splitext(INPUT_FILE)[0]
-OUTPUT_FILE = f"{FILENAME}.PNG"
+FILENAME = os.path.splitext(os.path.basename(INPUT_FILE))[0]
+
+# Especifica la ruta de salida aquí
+OUTPUT_DIR = "../public/final"  
+
+# Crear la carpeta de salida si no existe
+if not os.path.exists(OUTPUT_DIR):
+    os.makedirs(OUTPUT_DIR)
+
+OUTPUT_FILE = os.path.join(OUTPUT_DIR, f"{FILENAME}.png")
 
 # Leer el archivo binario y convertirlo a un array de NumPy
-width, height = 1024, 1024  # Asegúrate de que estas dimensiones coincidan con las usadas en el programa C
-array_imagen = np.fromfile(INPUT_FILE, dtype='int32').reshape((height, width))
+array_imagen = np.fromfile(INPUT_FILE, dtype='int32')
+
+# Determinar las dimensiones adecuadas del archivo BIN
+size = int(np.sqrt(len(array_imagen)))
+array_imagen = array_imagen.reshape((size, size))
 
 # Convertir el array a una imagen en escala de grises
 imagen = Image.fromarray(array_imagen.astype('uint8'))
@@ -22,3 +32,4 @@ imagen = Image.fromarray(array_imagen.astype('uint8'))
 # Guardar la imagen en formato PNG
 imagen.save(OUTPUT_FILE)
 
+print(f"Imagen guardada en {OUTPUT_FILE}")
